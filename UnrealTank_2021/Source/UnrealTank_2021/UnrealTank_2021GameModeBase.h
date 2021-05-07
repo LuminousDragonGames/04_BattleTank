32f8da83e9de8ede6dc8698d05b0ddc013d1fe6a
SundererKing <sundererking@gmail.com>
2021-05-07 08:41:00 -0700

    #125

---
+++ UnrealTank_2021/Source/UnrealTank_2021/UnrealTank_2021GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealTank_2021GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALTANK_2021_API AUnrealTank_2021GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
